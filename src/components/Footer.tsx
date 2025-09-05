8a67130ed30b707274e357740fa01ce658e2109b
Joe Canadas <joecanadas@gmail.com>
2025-09-05 12:00:17 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,9 +6,9 @@ export const Footer: React.FC = () => {
   return (
     <div className={styles.container}>
       <p>
-        Event Organized by Max Richards
+        Event Organized by Casey Malone
         <br />
-        Website by{' '}
+        Website forked from{' '}
         <a href="https://bsky.app/profile/anthony.luckypaper.co">
           Anthony Mattox
         </a>
